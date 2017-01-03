7e1a85194686a068974ec6344ebe051b919ddc46
Jesus.Ramos <Jesus.Ramos@Hmt-131.hmt.local>
2017-01-03 17:50:16 -0500

    [+] Logue correcto con manejo de sesion a traves de un objeto usuario.

--- src/controladores/InicioController.java
+++ src/controladores/InicioController.java
@@ -12,7 +12,6 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-import javafx.stage.Stage;
 
 /**
  * FXML Controller class
